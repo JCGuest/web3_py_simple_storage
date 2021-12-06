e7ad1ba9ef29a3f64228a5a501efa05e1fff3e33
JCGuest <jcguest90@gmail.com>
2021-12-05 18:29:28 -0600

    compile with solcx

--- deploy.py
+++ deploy.py
@@ -1,3 +1,25 @@
+from solcx import compile_standard
+import json
+
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
+
+# Compile our Solididty contract
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
