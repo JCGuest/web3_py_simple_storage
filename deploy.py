e3ea22eb01a299f818c45b33d589c6509b694a2f
JCGuest <jcguest90@gmail.com>
2021-12-06 22:01:48 -0600

    creat and deploy trxn

--- deploy.py
+++ deploy.py
@@ -1,5 +1,13 @@
-from solcx import compile_standard
+from solcx import compile_standard, install_solc
 import json
+from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+print("Installing...")
+install_solc("0.6.0")
+
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -20,6 +28,35 @@ compiled_sol = compile_standard(
     },
     solc_version="0.6.0",
 )
-
+# Save comiled contract to json file
 with open("compiled_code.json", "w") as file:
-    json.dump(compiled_sol, file)
+    file.write(json.dumps(compiled_sol))
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# connect to local testrpc
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0x83Eb48701044b65e8Cf32D29947659fF7CBc455C"
+private_key = os.getenv("PRIVATE_KEY")
+
+# Create contract
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+nonce = w3.eth.getTransactionCount(my_address)
+# Build, sign and send transaction
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+signed_txn = w3.eth.account.signTransaction(transaction, private_key)
+tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+print("tx_hash:", tx_hash)
