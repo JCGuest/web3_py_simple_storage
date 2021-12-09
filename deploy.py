5fe3744e4b524db8e29423f17f51a8bd88fec07a
JCGuest <jcguest90@gmail.com>
2021-12-09 10:10:59 -0600

    change state, add comments

--- deploy.py
+++ deploy.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-print("Installing...")
+print()
 install_solc("0.6.0")
 
 
@@ -40,7 +40,7 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # connect to local testrpc
-w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:8545"))
+w3 = Web3(Web3.HTTPProvider(os.getenv("SERVER")))
 chain_id = w3.eth.chainId
 my_address = os.getenv("MY_ADDRESS")
 private_key = os.getenv("PRIVATE_KEY")
@@ -60,16 +60,19 @@ transaction = SimpleStorage.constructor().buildTransaction(
 # Sign transaction
 signed_txn = w3.eth.account.signTransaction(transaction, private_key)
 # Send transaction
+print("Deploying Contract...")
 tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
 tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+print("Contract Deployed")
 
 # Working with contract
 simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
 
 # initial value of favorite number
-print(simple_storage.functions.retrieve().call())
-
-store_txn = simple_storage.functions.store(42).buildTransaction(
+# print(simple_storage.functions.retrieve().call())
+favNum = 42
+print(f"Updating favorite number to {favNum}")
+store_txn = simple_storage.functions.store(favNum).buildTransaction(
     {
         "chainId": chain_id,
         "from": my_address,
@@ -80,4 +83,6 @@ store_txn = simple_storage.functions.store(42).buildTransaction(
 signed_store_txn = w3.eth.account.signTransaction(store_txn, private_key)
 send_strore_txn = w3.eth.sendRawTransaction(signed_store_txn.rawTransaction)
 txn_receipt = w3.eth.waitForTransactionReceipt(send_strore_txn)
-print(txn_receipt)
+print("Update complete")
+print(f"Favorite number is {simple_storage.functions.retrieve().call()}")
+print(f"Transaction reciept:{txn_receipt}")
